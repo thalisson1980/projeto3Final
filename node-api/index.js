6d82be77352495d73cd6bd04eebe5e5566ba1c45
Márcia Regina Bernardo <79754243+Marcia-Bernardo@users.noreply.github.com>
2022-02-03 17:48:57 +0000

    router adicionada

--- node-api/index.js
+++ node-api/index.js
@@ -1,38 +1,37 @@
 const express = require('express');
-const bodyParser = require ('body-parser');
+const bodyParser = require('body-parser');
 const cors = require('cors');
-
-
-
+const app = express();
 
 const db = require('../node-api/bd/ligacao');
 
-const app = express();
-
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.text());
 app.use(cors());
 
-//definir rotas
-const userRoute = require('./controllers/user');
-app.use('/user',userRoute);
-const keyRoute = require('./controllers/keyController');
-app.use('/key',keyRoute);
-const keyRequestRoute = require('./controllers/keyRequestController');
-app.use('/keyRequest', keyRequestRoute);
-const containerRoute = require('./controllers/containerController');
-app.use('/container',containerRoute);
-const collectionRoute = require('./controllers/collectionController');
-app.use('/collection',collectionRoute);
-const containerCollectionRoute = require('./controllers/containerCollectionController');
-app.use('/containerCollection',containerCollectionRoute);
-const DDCCFFRoute = require('./controllers/ddccffController');
-app.use('/DDCCFF',DDCCFFRoute);
+
+const router = require("./router/routes");
+app.use(router);
+// //definir rotas
+// const userRoute = require('./controllers/user');
+// app.use('/user',userRoute);
+// const keyRoute = require('./controllers/keyController');
+// app.use('/key',keyRoute);
+// const keyRequestRoute = require('./controllers/keyRequestController');
+// app.use('/keyRequest', keyRequestRoute);
+// const containerRoute = require('./controllers/containerController');
+// app.use('/container',containerRoute);
+// const collectionRoute = require('./controllers/collectionController');
+// app.use('/collection',collectionRoute);
+// const containerCollectionRoute = require('./controllers/containerCollectionController');
+// app.use('/containerCollection',containerCollectionRoute);
+// const DDCCFFRoute = require('./controllers/ddccffController');
+// app.use('/DDCCFF',DDCCFFRoute);
 
 
-  
 
-app.listen(3000,(req,res) =>{
+
+app.listen(3000, (req, res) => {
     console.log("Servidor a ouvir na porta 3000");
 });
---
+++ node-api/router/routes.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+const router = express.Router();
+
+
+const userRoute = require('.././controllers/user');
+const keyRoute = require('.././controllers/keyController');
+const keyRequestRoute = require('.././controllers/keyRequestController');
+
+const DDCCFFRoute = require('.././controllers/ddccffController');
+const reqAuthRoute = require('.././controllers/reqAuthController');
+const EmployeeRoute = require('.././controllers/employeeController');
+const circuitRoute = require('.././controllers/circuitController');
+const containerRoute = require('.././controllers/containerController');
+const collectionRoute = require('.././controllers/collectionController');
+const containerCollectionRoute = require('.././controllers/containerCollectionController');
+
+
+
+router.use('/DDCCFF', DDCCFFRoute);
+router.use('/user', userRoute);
+
+router.use('/key', keyRoute);
+router.use('/circuit', circuitRoute);
+router.use('/keyRequest', keyRequestRoute);
+router.use('/employee', EmployeeRoute);
+router.use('/collection', collectionRoute);
+router.use('/container', containerRoute);
+router.use('/containerCollection', containerCollectionRoute);
+router.use('/reqAuth', reqAuthRoute);
+
+module.exports = router;
