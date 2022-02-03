be62e1a0ed9c81339ab637890552f3e4c319daa5
Márcia Regina Bernardo <79754243+Marcia-Bernardo@users.noreply.github.com>
2022-02-03 18:54:03 +0000

    read_employee

--- node-api/index.js
+++ node-api/index.js
@@ -4,14 +4,14 @@ const cors = require('cors');
 const app = express();
 const router = require("./router/routes");
 
-const db = require('../node-api/bd/ligacao');
+const db = require('./bd/ligacao');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.text());
 app.use(cors());
 app.use(router);
-
+// app.use(db);
 
 app.listen(3000, (req, res) => {
     console.log("Servidor a ouvir na porta 3000");
--- node-api/models/user.js
+++ node-api/models/user.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
+    _id: {
+        type: String,
+    },
     name: {
         type: String,
         required: true
